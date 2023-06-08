843a704a08541dbe9d24639e4a400741aeb9e436
teepatra <fagboretomilola@gmail.com>
2023-06-08 13:07:16 -0700

    updating file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """print the addition of all arguments."""
     import sys
-
     sum = 0
-    for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv[i + 1])
-        print("{}".format(sum))
+    lst = sys.argv
+    for i in range(1, len(lst)):
+        sum = sum + int(lst[i])
+    print(sum)
