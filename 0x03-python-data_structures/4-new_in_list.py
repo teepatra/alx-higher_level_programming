6eea791bb408607c635909c81771dca0c4f3e9c5
teepatra <fagboretomilola@gmail.com>
2023-06-12 07:53:53 -0700

    replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return my_list:
+            else:
+                new_list = []
+                for i in my_list:
+                    if my_list.index(i) == idx:
+                        new_list.append(element)
+                    else:
+                        new_list.append(i)
+                        return new_list
