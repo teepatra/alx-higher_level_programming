f06ed44ff8db82d3acbbf3dc0a32b36cc4a74a4d
teepatra <fagboretomilola@gmail.com>
2023-06-14 14:06:32 -0700

    fix file for best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary is {}:
-        return None
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return (None)
     else:
-        return max(a_dictionary)
+        v = list(a_dictionary.values())
+        k = list(a_dictionary.keys())
+        return (k[v.index(max(v))])
