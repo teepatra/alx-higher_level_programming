af64010e1e3503a19c550781b587cd37ac295594
teepatra <fagboretomilola@gmail.com>
2023-07-05 15:39:48 +0000

    correcting nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,19 +5,9 @@ N Queens puzzle module
 """
 
 import sys
-
-def main(argv):
-    if len(argv) is not 1:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    N = int(argv[0])
-    if type(N) is not int:
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    print(int(argv[0]))
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+def init_chess(n):
+    """Initialize  `n`x`n` chess size with 0's."""
+    chess = []
+    [chess.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in chess]
+    return (chess)
