390ad670a56eab40d4df86d298b1ccbb7891ae5c
teepatra <fagboretomilola@gmail.com>
2023-07-10 13:55:59 +0000

    list of available attributes and methods of an object:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Module to lookup for available methds"""
 
 def lookup(obj):
     """
     Returns a list of the names of the attributes and methods of an object.
 
-    Parameters:
+    Args:
     obj (object): The object whose attributes and methods to list.
 
     Returns:
-    list: A list of strings representing the names of the attributes and methods of the object.
+    (list): A list of object.
     """
     return dir(obj)
