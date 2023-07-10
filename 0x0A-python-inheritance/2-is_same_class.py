8b1e78940335a8e4b70c75312efb92d6ec7de53d
teepatra <fagboretomilola@gmail.com>
2023-07-10 14:40:18 +0000

    Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
+""" This module contains is same class"""
 
+def is_same_class(obj, a_class):
+    """return true if obj is the exact class a_class, otherwise false"""
+    return (type(obj) == a_class)
